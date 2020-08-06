6796b224175e617be635a29815fc7b0ad4a722ee
CharlesOv0 <sunc1434@gmail.com>
2020-08-06 09:41:20 +0800

    some review

--- diy/unique_ptr.hpp
+++ diy/unique_ptr.hpp
@@ -3,38 +3,45 @@
 namespace diy {
 template <typename T>
 class unique_ptr {
- public:
-  explicit unique_ptr(T *ptr) : raw_ptr_(ptr) {}
-  unique_ptr(unique_ptr &&other) noexcept {
-    raw_ptr_ = other.raw_ptr_;
-    other.raw_ptr_ = nullptr;
-  };
-  //   unique_ptr &operator=(unique_ptr &&other) noexcept {
-  //     if (&other != this) std::swap(other.raw_ptr_, raw_ptr_);
-  //     return *this;
-  //   }
+ private:
+  T *ptr_;
 
-  // move and swap  idiom replace the move assignment operator
-  unique_ptr &operator=(unique_ptr rhs) noexcept  // will call move constructor
-  {
-    std::swap(raw_ptr_, rhs.raw_ptr_);
-    // std::swap(*this,rhs)  // is also ok
+ public:
+  explicit unique_ptr(T *ptr) : ptr_(ptr) {}
+  unique_ptr(unique_ptr &&other) noexcept : ptr_(nullptr) { this->swap(other); };
+  unique_ptr &operator=(unique_ptr &&other) noexcept {
+    this->swap(other);
     return *this;
   }
+
+  // move and swap  idiom replace the move assignment operator
+  // unique_ptr &operator=(unique_ptr rhs) noexcept  // will call move constructor
+  // {
+  //   // std::swap(ptr_, rhs.ptr_);// is also ok
+  //   this->swap(rhs);
+  //   return *this;
+  // }
   unique_ptr(const unique_ptr &) = delete;
-  //  unique_ptr &operator=(const unique_ptr &) = delete; // shouldn't reserved
+  unique_ptr &operator=(const unique_ptr &) =
+      delete;  // note: shouldn't be reserved when use move-swap
+
   ~unique_ptr() {
-    delete raw_ptr_;
-    raw_ptr_ = nullptr;
+    delete ptr_;
+    ptr_ = nullptr;
   }
 
-  T *get() { return raw_ptr_; }
+  void swap(unique_ptr &other) noexcept {
+    using std::swap;
+    swap(ptr_, other.ptr_);
+  }
 
-  T &operator*() const { return *raw_ptr_; }
+  T *get() const { return ptr_; }
 
-  T *operator->() const { return raw_ptr_; }
+  T &operator*() const { return *ptr_; }
 
- private:
-  T *raw_ptr_;
+  T *operator->() const { return ptr_; }
+
+  // So it can be used in conditional expression
+  explicit operator bool() { return ptr_; }
 };
 }  // namespace diy
